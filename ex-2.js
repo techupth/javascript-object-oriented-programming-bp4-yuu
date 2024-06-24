41138ceb28f87887d9355b2cd89f82743b1ad162
Swpee <s.suwapee@gmail.com>
2024-06-24 16:40:50 +0700

    add: Assignment : JavaScript Object-Oriented Programming Assignment

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,32 @@
-class Notification { }
 
-class EmailNotification { }
-
-class SMSNotification { }
+class Notification {
+    constructor (notificationID, createdTime, content, receiver) {
+        this.notificationID = notificationID;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+  
+    send () {
+        return console.log(`Notification has been sent to ${this.receiver}`)
+    }
+  }
+  
+  class EmailNotification extends Notification {
+    constructor (notificationID, createdTime, content, receiver) {
+        super (notificationID, createdTime, content,receiver)
+    }
+  }
+  
+  class SMSNotification extends Notification {
+    constructor (notificationID, createdTime, content, phoneNumber) {
+      super (notificationID, createdTime, content)
+        this.phoneNumber = phoneNumber;
+    }
+  }
+  
+  let deliveryEmailNotification = new EmailNotification (1,"09:48","Delivery Complete","s.suwapee@gmail.com")
+  let deliverySMSNotification = new EmailNotification (1,"09:48","Delivery Complete","0895528856")
+  
+  deliveryEmailNotification.send()
+  deliverySMSNotification.send()
